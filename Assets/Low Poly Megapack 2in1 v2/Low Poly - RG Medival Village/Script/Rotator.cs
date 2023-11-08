8fd2e87eb60d6d65a0bd82083434e17204302104
TheWhitcher <zwhitcher1995@gmail.com>
2023-11-08 14:38:36 -0500

    Credit card-kun saved the day buying terrain assets.

---
+++ Assets/Low Poly Megapack 2in1 v2/Low Poly - RG Medival Village/Script/Rotator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] private Vector3 _rotation;
+    [SerializeField] private float _speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(_rotation * _speed * Time.deltaTime);
+    }
+}
